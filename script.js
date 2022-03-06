00c2e404571fd7002f72858c60a75bcc8b949657
“Rebecca <rebeccaschiffman@gmail.com>
2022-03-05 17:19:30 -0800

    Create function to check for winner or tie

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 
 
-const setUpArea = document.getElementById('setuparea');
-const newGameButton = document.getElementById('newgamebutton');
+//DOM VARIABLES
+
+const newGameBtnDiv = document.getElementById('newgamebtndiv');
 const playerChoiceArea = document.querySelector('#playerchoicearea');
 
-const onePlayerButton = document.getElementById('oneplayerbutton');
-const onePlayerFormContainer = document.getElementById('oneplayerformcontainer');
+//new game button opens div to select further options before starting game
+const newGameButton = document.getElementById('newgamebutton');
 
+//buttons to choose one player or two player game 
+const onePlayerButton = document.getElementById('oneplayerbutton');
 const twoPlayerButton = document.getElementById('twoplayerbutton');
+
+//forms for one player name and difficulty level, and two player names
+const onePlayerFormContainer = document.getElementById('oneplayerformcontainer');
 const twoPlayerFormContainer = document.getElementById('twoplayerformcontainer');
 
+//buttons to start one and two player games
 const startButton1 = document.getElementById('startbutton1');
 const startButton2 = document.getElementById('startbutton2');
 
 const gameBoardContainer = document.getElementById('gameboardcontainer');
 
+
 // UTILITY FUNCTIONS
 
+// Function to show one element
 function displayElement(element, display){
     element.style.display = display;
 }
@@ -44,7 +53,7 @@ function removeAllChildNodes(parent) {
 //when you click new game
 newGameButton.addEventListener('click', function(){
     displayElement(playerChoiceArea, 'block');
-    hideElement(setUpArea);
+    hideElement(newGameBtnArea);
 });
 
 // when you click one player button
@@ -102,21 +111,117 @@ startButton1.addEventListener('click', function(){
 
 const gameBoard = function() {
 
+    //Filling in default X's and O's for now.
     var gameBoardArray = ['X','O','X','O','X','O','X','O','X'];
-    console.log(gameBoardArray.length); 
+    
+    //Counting variable for naming the cell ID's
     let a=1;
+
     //for each item in gameBoardArray
     gameBoardArray.forEach((i) => {
+        //create a div and give it class and ID
         var cell = document.createElement('div');
         cell.classList.add('cell');
         cell.id=`cell-${a}`;
+
+        //put the current array item in each cell
         cell.textContent=i;
+
+        //add the div as a cell to the game board grid
         gameBoardContainer.appendChild(cell);
+
+        //increase counting variable
         a=a+1;
-        // console.log(gameBoardArray[i]);
         
     });
 }
+
+
+//function to check for winners
+const checkForWinners = function() {
+
+    //create arrays of rows to check
+    const rowOneArray = gameBoardArray.slice(0,3);
+    const rowTwoArray = gameBoardArray.slice(3,6);
+    const rowThreeArray = gameBoardArray.slice(6);
+    const columnOneArray = [gameBoardArray.at(0), gameBoardArray.at(3), gameBoardArray.at(6)];
+    const columnTwoArray = [gameBoardArray.at(1), gameBoardArray.at(4), gameBoardArray.at(7)];
+    const columnThreeArray = [gameBoardArray.at(2), gameBoardArray.at(5), gameBoardArray.at(8)];
+    const diagonalOneArray = [gameBoardArray.at(0), gameBoardArray.at(4),gameBoardArray.at(8)];
+    const diagonalTwoArray = [gameBoardArray.at(2), gameBoardArray.at(4),gameBoardArray.at(6)];
+
+    //should I then make an array of these arrays?
+    const rowsToCheckArray = [
+        rowOneArray,
+        rowTwoArray,
+        rowThreeArray,
+        columnOneArray,
+        columnTwoArray,
+        columnThreeArray,
+        diagonalOneArray,
+        diagonalTwoArray,
+    ]
+
+    //utility function - check if all items in an array are equal
+    const allEqual = arr => arr.every(value => value === arr[0]);
+
+    // utlity function - check if all items in array are full
+    const allFull = arr => arr.every(value => value === 'X' || value === 'O');
+
+    //FOR EACH LOOP
+    //for every stretch of gameboard where someone could win
+    rowsToCheckArray.forEach((array) => {
+
+        //if there are three x's or three o's in a row there's a winner 
+        if (allEqual(array) == true) {
+            console.log('There is a winner');
+
+        //if the gameboard is full and there are not three in a row it's a tie
+        } else if (allFull(array) == true && allEqual(array) == false) {
+            console.log('It\'s a tie!');
+
+        //otherwise keep playing
+        } else {
+            continue;
+        }
+    });
+}
+    
+    
+
+
+
+//Function to let player fill in grid
+//when you click on gameBoardContainer
+    gameBoardContainer.onclick = function(event) {
+
+        //figure out which cell was clicked
+        let target = event.target;
+
+        //if cell is empty and has classname cell
+        if (target.innerText='' && event.target.className === 'cell') {
+
+            //put current playerText into the array index 
+            //location of that cell
+            gameBoardArray[event]='x';
+            target.innerText = 'x' ;
+            //or should I just run
+            //gameBoard() 
+
+        } else {
+            return;
+        }
+    }
+
+
+
+
+
+
+
+//GAME FLOW OBJECT
+
+//when 
 
 
 
@@ -157,7 +262,7 @@ const gameBoard = function() {
 
     //     const winnerMessage = () => `${name} is the winner!`;
     //     const scorePlusOne = () => score += 1;
-    //     const getTeam = team => (team = 'x') ? 'x' : 'o';
+    //     const getTeamValue = team => (team = 'x') ? 'x' : 'o';
 
     //     return {
     //         name, // this is shorthand for name: name
