aa349c4d354dfa950526f3919d910d31b70d808e
“Rebecca <rebeccaschiffman@gmail.com>
2022-03-07 14:04:18 -0800

    Work on gameFlow object

--- script.js
+++ script.js
@@ -83,35 +83,24 @@ twoPlayerButton.addEventListener('click', function(){
 });
 
 
-
-
-
-
 //FACTORY FUNCTION TO CREATE PLAYERS
-const playerFactory = (name,letter) => {
-    const playerText = letter;
-    const turnMessage = () => `${name}'s turn`; 
-    const winnerMessage = () => `${name} is the winner!`;
+const playerFactory = (name, playerText) => {
+    const turnMessage = `${name}'s turn`; 
+    const winnerMessage = `${name} is the winner!`;
     return {
         name,
         playerText,
         turnMessage,
         winnerMessage,
     }
-}
+};
 
-
-//DISPLAY CONTROLS OBJECT
-const displayControls = (() => {
-});
 
 //GAMEBOARD OBJECT
 const gameBoard = (() => {
 
     //create array with default text
     var gameBoardArray = ['T','I','C','T','A','C','T','O','E'];
-
-    
 
     // Function to create cells in gameBoard grid
     const createCells = function() {
@@ -122,7 +111,7 @@ const gameBoard = (() => {
             cell.classList.add('cell');
             cell.id=`cell-${step}`;
             gameBoardContainer.appendChild(cell);
-            console.log(cell.id);
+            // console.log(cell.id);
           }
     }
 
@@ -136,201 +125,155 @@ const gameBoard = (() => {
         let b=1;
         array.forEach((i) => {
             var currentCell = document.getElementById(`cell-${b}`);
-            console.log(`a is ${b} and i is ${i}`)
-            console.log(currentCell.id);
-            // currentCell.textContent=' ';
+            // console.log(`a is ${b} and i is ${i}`)
+            // console.log(currentCell.id);
             currentCell.textContent=i;
             b=b+1;
         });
     }
+
+//function to check for winners or tie
+const checkForWinners = function() {
+
+    //utility function - check if all items in an array are equal
+    const allEqual = arr => arr.every(value => value === arr[0]);
+
+    // utlity function - check if all items in array are full
+    const allFull = arr => arr.every(value => value === 'X' || value === 'O');
+     
+   //create arrays of rows to check
+   const rowOneArray = gameBoardArray.slice(0,3);
+   const rowTwoArray = gameBoardArray.slice(3,6);
+   const rowThreeArray = gameBoardArray.slice(6);
+   const columnOneArray = [gameBoardArray.at(0), gameBoardArray.at(3), gameBoardArray.at(6)];
+   const columnTwoArray = [gameBoardArray.at(1), gameBoardArray.at(4), gameBoardArray.at(7)];
+   const columnThreeArray = [gameBoardArray.at(2), gameBoardArray.at(5), gameBoardArray.at(8)];
+   const diagonalOneArray = [gameBoardArray.at(0), gameBoardArray.at(4),gameBoardArray.at(8)];
+   const diagonalTwoArray = [gameBoardArray.at(2), gameBoardArray.at(4),gameBoardArray.at(6)];
+
+   //should I then make an array of these arrays?
+   const rowsToCheckArray = [
+       rowOneArray,
+       rowTwoArray,
+       rowThreeArray,
+       columnOneArray,
+       columnTwoArray,
+       columnThreeArray,
+       diagonalOneArray,
+       diagonalTwoArray,
+   ]
+
+   //FOR EACH LOOP
+   //for every stretch of gameboard where someone could win
+   rowsToCheckArray.forEach((array) => {
+
+       //if there are three x's or three o's in a row there's a winner 
+       if (allEqual(array) == true) {
+           console.log('There is a winner');
+           //will fill in later how to check who won
+
+       //if the gameboard is full and there are not three in a row it's a tie
+       } else if (allFull(array) == true && allEqual(array) == false) {
+           console.log('It\'s a tie!');
+
+       //otherwise keep playing
+       } else {
+           return;
+       }
+   });
+}
 
     return {
         gameBoardArray,
         createCells,
         makeArrayBlank,
         fillCells,
-    }
-    
+        checkForWinners,
+    }  
 })();
+
+
 
 // GAME FLOW
 const gameFlow = (() => {
 
-    //fill in game flow stuff here
+    //function to let a user place a letter
+    const makeAMove = function(event){
+
+        let target = event.target;
+        console.log(target);
+        //if cell is empty and has classname cell
+        if (target.innerText=' ' && event.target.className === 'cell') {
+            //put current playerText into the array index 
+            indexNum = Number(target.id.slice(-1))-1;
+            console.log(gameBoard.gameBoardArray[indexNum]);
+
+            //later change to 
+            //gameBoard.gameBoardArray[indexNum] = currentPlayer.playerText;
+            gameBoard.gameBoardArray[indexNum] = 'X';
+            console.log(gameBoard.gameBoardArray[indexNum]);
+
+            //later change to
+            //target.innerText = currentPlayer.playerText;
+            target.innerText = 'X' ;
+            switchPlayers();
+        } else {
+            return;
+        }
+    }
+
+    const startTwoPlayerGame = function(){
+        //hide the form you just filled out
+        hideElement(twoPlayerFormContainer);
+        //get name of playerOne from the form input
+        const playerOneName = document.getElementById('player1name2').value;
+        console.log(playerOneName);
+        //get name of playerTwo from the form input
+        const playerTwoName = document.getElementById('player2name').value;
+        console.log(playerTwoName);
+        //create playerOne using factory function and assign to X
+        const playerOne = playerFactory(playerOneName,'X');
+        console.log(`Player One is named ${playerOne.name}`)
+        //create playerTwo using factory function and assign to O
+        const playerTwo = playerFactory(playerTwoName,'O');
+        console.log(`Player Two is named ${playerTwo.name}`)
+        //set first turn at player one's turn
+        const currentPlayer = playerOne;
+        //write that it's playerOne's turn
+        whoseTurn.textContent=playerOne.turnMessage;
+        // whoseTurn.textContent=`${playerOneName}'s turn.`;
+        gameBoard.makeArrayBlank(gameBoard.gameBoardArray);
+        gameBoard.fillCells(gameBoard.gameBoardArray);
+        gameBoardContainer.classList.add('gameboardcontainer-active');
+        gameBoardContainer.addEventListener('click', makeAMove);
+        // switchPlayers;
+        // whoseTurn.textContent=playerTwo.turnMessage;
+    }
+
+    startButton2.addEventListener('click', startTwoPlayerGame);
+
+    gameBoardContainer.addEventListener('click', makeAMove);
+
+     //function to switch players
+     const switchPlayers = function(){
+        if (currentPlayer = playerOne) {
+            currentPlayer = playerTwo;
+        } else if (currentPlayer = playerTwo) {
+            currentPlayer = playerOne;
+        }
+        console.log(currentPlayer.turnMessage);
+    }
+
+    return{
+        playerOne,
+        playerTwo,
+    }
 
 })();
 
-
-
-
-startButton2.addEventListener('click', function(){
-    console.log('start 2 button was pushed');
-    //hide the form you just filled out
-    hideElement(twoPlayerFormContainer);
-    //get name of playerOne
-    var playerOneName = document.getElementById('player1name2').value;
-    console.log(playerOneName);
-    //get name of playerTwo
-    var playerTwoName = document.getElementById('player2name').value;
-    console.log(playerTwoName);
-    //create player1 and assign to X
-    // const player1 = playerFactory(playerOneName,'X');
-    // //create player2 and assign to O
-    // const player2 = playerFactory(playerTwoName,'O');
-    // //set first turn at player one's turn
-    // const playerTurn = '1';
-    //write it's player one's turn
-    whoseTurn.textContent=`${playerOneName}'s turn.`;
-    gameBoard.makeArrayBlank(gameBoard.gameBoardArray);
-    gameBoard.fillCells(gameBoard.gameBoardArray);
-    gameBoardContainer.classList.add('gameboardcontainer-active');
-})
-
-
-
-//this did not work
-// startButton2.addEventListener('click',startTwoPlayerGame);
-// const startTwoPlayerGame = function(){
-//     console.log('start 2 button was pushed');
-//     //hide the form you just filled out
-//     hideElement(twoPlayerFormContainer);
-// }
-
-
-//     //get name of playerOne
-//     var playerOneName =  document.getElementById('player1name').value;
-//     //get name of playerTwo
-//     var playerTwoName = document.getElementById('player2name').value;
-//     //create player1 and assign to X
-//     const player1 = playerFactory(playerOneName,'X');
-//     //create player2 and assign to O
-//     const player2 = playerFactory(playerTwoName,'O');
-//     //set first turn at player one's turn
-//     const playerTurn = '1';
-//     //write it's player one's turn
-//     whoseTurn.textContent=`${player1}'\s turn.`;
-//     //makeArrayBlank - at some point createCells was already run,
-//     // maybe in the controls object
-//     gameBoard.makeArrayBlank(gameBoard.gameBoardArray);
-//     //
-// }
-
-//Function to let player fill in grid
-// //when you click on gameBoardContainer
-//     gameBoardContainer.addEventListener("click",makeAMove) 
-
-//     const makeAMove = function(event) {
-//         //figure out which cell was clicked
-//         let target = event.target;
-//         //if cell is empty and has classname cell
-//         if (target.innerText='' && event.target.className === 'cell') {
-//             //put current playerText into the array index 
-//             //location of that cell
-//             gameBoardArray[event]='x';
-//             target.innerText = 'x' ;
-//             //or should I just run
-//             //gameBoard() 
-//         } else {
-//             return;
-//         }
-//     }
-
-
-
-
-
-
-
-//function to check for winners
-const checkForWinners = function() {
-
-     //utility function - check if all items in an array are equal
-     const allEqual = arr => arr.every(value => value === arr[0]);
-
-     // utlity function - check if all items in array are full
-     const allFull = arr => arr.every(value => value === 'X' || value === 'O');
- 
-
-    //create arrays of rows to check
-    const rowOneArray = gameBoardArray.slice(0,3);
-    const rowTwoArray = gameBoardArray.slice(3,6);
-    const rowThreeArray = gameBoardArray.slice(6);
-    const columnOneArray = [gameBoardArray.at(0), gameBoardArray.at(3), gameBoardArray.at(6)];
-    const columnTwoArray = [gameBoardArray.at(1), gameBoardArray.at(4), gameBoardArray.at(7)];
-    const columnThreeArray = [gameBoardArray.at(2), gameBoardArray.at(5), gameBoardArray.at(8)];
-    const diagonalOneArray = [gameBoardArray.at(0), gameBoardArray.at(4),gameBoardArray.at(8)];
-    const diagonalTwoArray = [gameBoardArray.at(2), gameBoardArray.at(4),gameBoardArray.at(6)];
-
-    //should I then make an array of these arrays?
-    const rowsToCheckArray = [
-        rowOneArray,
-        rowTwoArray,
-        rowThreeArray,
-        columnOneArray,
-        columnTwoArray,
-        columnThreeArray,
-        diagonalOneArray,
-        diagonalTwoArray,
-    ]
-
-    //FOR EACH LOOP
-    //for every stretch of gameboard where someone could win
-    rowsToCheckArray.forEach((array) => {
-
-        //if there are three x's or three o's in a row there's a winner 
-        if (allEqual(array) == true) {
-            console.log('There is a winner');
-
-        //if the gameboard is full and there are not three in a row it's a tie
-        } else if (allFull(array) == true && allEqual(array) == false) {
-            console.log('It\'s a tie!');
-
-        //otherwise keep playing
-        } else {
-            // continue;
-        }
-    });
-}
-    
-    
-
-
-//GAME FLOW OBJECT
-
-//when you press start game
-    //playerTurn = player one
-    //display it's player one's tur
-
-
-//play game fuctiom    
-    
-    //when you click a square, get the id of that square
-
-    //player.turnMessage();
-
-    //if playerTurn is playerOne
-    // if (playerTurn = player1){
-    //     console.log(playerTurn);
-    //     whoseTurn.textContent=`${player2}'\s turn.`;
    
-    //     // input X to the gameboard array and 
-    //     // input x to cell's textCotetn
-    //     //check if there's a winner or tie
-    //     //if game is still going
-    //         //playerTurn = player two
-    //         //display it's player two's turn
-    //         //return
-    //   //else if playerTurn is playerTwo
-    // } else if (playerTurn = player2) {
-    //     console.log(playerTurn);
-    //     whoseTurn.textContent=`${player1}'\s turn.`;
-   
-  
-        // input O to the gameboard array and 
-        // input O to cell's textCotetn
-        //check if there's a winner or tie
-        //if game is still going
-            //playerTurn = player one
-            //display it's player two's turn
-            //return
-    // }
+
+// //DISPLAY CONTROLS OBJECT
+// const displayControls = (() => {
+// });
+
